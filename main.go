cb7b9349ab3151dcdee1e63ca8ca80d5a6e5475b
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-24 15:22:53 -0500

    returning windows

--- main.go
+++ main.go
@@ -12,11 +12,14 @@ func main() {
 	fmt.Println(res)
 }
 
+//TODO: Add varadic variables to path.Join
 func appDir() string {
 	switch runtime.GOOS {
 	case "darwin":
 		return path.Join(os.Getenv("HOME"), "Library", "Application Support")
+	case "windows":
+		return os.Getenv("APPDATA")
 	default:
-		return "Error"
+		return runtime.GOOS
 	}
 }

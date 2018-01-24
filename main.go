0f19abeca119ccb97954d283a7a121969367e817
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-24 17:50:44 -0500

    Exporting

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
-package main
+package appdir
 
 import (
-	"fmt"
 	"os"
 	"path"
 	"runtime"
 )
 
 func main() {
-	res := appDir()
-	fmt.Println(res)
+	// res := appDir()
+	// fmt.Println(res)
 }
 
 //TODO: Add varadic variables to path.Join
-func appDir() string {
+func Is() string {
 	switch runtime.GOOS {
 	case "darwin":
 		return path.Join(os.Getenv("HOME"), "Library", "Application Support")

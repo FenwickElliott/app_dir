56f15032f95137dbce080ee55838db47c43904cc
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-01-24 15:03:44 -0500

    returning darwin

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path"
 	"runtime"
 )
 
 func main() {
-	fmt.Println(runtime.GOOS)
+	res := appDir()
+	fmt.Println(res)
+}
+
+func appDir() string {
+	switch runtime.GOOS {
+	case "darwin":
+		return path.Join(os.Getenv("HOME"), "Library", "Application Support")
+	default:
+		return "Error"
+	}
 }
